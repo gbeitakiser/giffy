92f937dae6ffc2ea0c895f2d75fe897dbff2f2ad
Gabriel Beita-Kiser <gbeitakiser@gmail.com>
2018-10-22 18:13:31 -0700

    Got gif to post on button press. Still needs everything else

--- Javascript/tinker.js
+++ Javascript/tinker.js
@@ -1,15 +1,18 @@
+$(document).ready(function(){
+
+
+
+
 // Vars
 
 var topics = ["Pulp Fiction", "Kill Bill", "Reservoir Dogs", "Jackie Brown", "Natural Born Killers", "Desperado", "Sin City", "Inglorious Bastards"]
 
-// Make "Subject" an empty field that you can append to once I get buttons looped
-var subject = "";
-// var btnValue = "";
 
-var giphyURL = "https://api.giphy.com/v1/gifs/search?q=" + subject + "&api_key=dc6zaTOxFJmzC&limit=10";
 
 // Document Ready
-$(document).ready(function() {
+function displayData() {
+    var subject = $(this).attr("data-name");
+    var giphyURL = "https://api.giphy.com/v1/gifs/search?q=" + subject + "&api_key=dc6zaTOxFJmzC&limit=10";
 
     // AJAX call
     $.ajax({
@@ -19,30 +22,26 @@ $(document).ready(function() {
         var results = response.data;
         console.log(response);
         console.log(results);
-
-        // var pic1 = $("#display").append('<img src="' + results[0].images.downsized_large.url + '">')
-        
-    })  
+        $("#display").append('<img src=' + results[0].images.fixed_height.url + "'><br><br>");
+    })
+};
 
     // $("#buttons").empty();
-    for (var i = 0; i < topics.length; i++) {
-        var buttonOrig = $("<button>");
-        buttonOrig.addClass("button");
-        buttonOrig.val(topics[i]);
-        buttonOrig.attr("data-name", topics[i]);
-        buttonOrig.text(topics[i]);
-        buttonValue = buttonOrig[0].value;
-        subject = buttonValue;
-
-        $("#buttons").append(buttonOrig);
-        // console.log(buttonValue);
-        console.log(subject);
-        
-       
+    function renderButtons() {
+        for (var i = 0; i < topics.length; i++) {
+            var buttonOrig = $("<button>");
+            buttonOrig.addClass("button");
+            buttonOrig.val(topics[i]);
+            buttonOrig.attr("data-name", topics[i]);
+            buttonOrig.text(topics[i]);
+            buttonValue = buttonOrig[0].value;
+            $("#buttons").append(buttonOrig);
+            console.log(buttonValue);
     }
-    console.log("-")
-    console.log(subject);
-  
-    
+    }
 
+    $(document).on("click", ".button", displayData);
+
+    // displayData();
+    renderButtons();
 });
